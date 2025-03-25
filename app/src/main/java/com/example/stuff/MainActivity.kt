f876d7e1e84c8c4c5ca75e4c08363ef485215c1a
justKeth <sixaruliketi@gmail.com>
2025-03-25 11:50:45 +0400

    project25.3.2025-2

---
+++ app/src/main/java/com/example/stuff/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.stuff
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            val intent = Intent(this, MoreStuff::class.java)
+            intent.putExtra("email", findViewById<EditText>(R.id.email).text.toString())
+            intent.putExtra("password", findViewById<EditText>(R.id.password).text.toString())
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/stuff/MoreStuff.kt
@@ -0,0 +1,24 @@
+package com.example.stuff
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
+
+class MoreStuff : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_more_stuff)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        findViewById<TextView>(R.id.stuff).text=intent.extras?.getString("email")
+        findViewById<TextView>(R.id.stuff2).text=intent.extras?.getString("password")
+    }
+}
